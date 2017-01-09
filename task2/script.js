3daa3330bfdbf9ff1ccdc41694f27c33834bafed
mish0y <mihael_mihalev@abv.bg>
2017-01-09 21:47:24 +0200

    готово
    готово

--- task2/script.js
+++ task2/script.js
@@ -3,16 +3,23 @@
 // задача 1
 
 var arr = [2, 5, 8, 4, 1, 12];
+arr.sort(function(a, b){return a-b});
 
 // задача 2
 
 var text = "The quick brown fox jumps over the lazy dog";
+var sep = text.split(" ");
+
+следва продължение... :) 
 
 // задача 3
 
 
 // задача 4
 
+$(document).ready(function() {
+    $('img').rotate(180).delay(2000);
+});
 
 // задача 5
 
